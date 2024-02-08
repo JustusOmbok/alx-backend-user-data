8655237e3a18038d2d5ff80c8286ab5f074368ee
JustusOmbok <ombokjustus@gmail.com>
2024-02-08 17:18:10 +0300

    obfuscating user data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -89,7 +89,7 @@ def get_db():
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    database = os.getenv("PERSONAL_DATA_DB_NAME")
+    database = os.getenv("PERSONAL_DATA_DB_NAME", "")
 
     return mysql.connector.connect(
         user=username,
