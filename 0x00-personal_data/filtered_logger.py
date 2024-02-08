b6dfa986eedefc88a556c8b572429e251148a014
JustusOmbok <ombokjustus@gmail.com>
2024-02-08 16:23:43 +0300

    obfuscating user data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,13 +17,16 @@ def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
     """
-    Filters specified fields in a log message with the provided redaction string.
+    Filters specified fields in a log message
+    with the provided redaction string.
 
     Args:
         fields: A list of strings representing all fields to obfuscate.
-        redaction: A string representing what the field will be obfuscated with.
+        redaction: A string representing
+        what the field will be obfuscated with.
         message: A string representing the log line.
-        separator: A string representing the character separating all fields in the log line.
+        separator: A string representing the character
+        separating all fields in the log line.
 
     Returns:
         A string representing the log line with specified fields obfuscated.
