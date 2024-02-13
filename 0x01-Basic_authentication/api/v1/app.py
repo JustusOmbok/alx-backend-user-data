27f2e52e7d46d0ca9a46cbd154ab757956956915
JustusOmbok <ombokjustus@gmail.com>
2024-02-12 23:16:06 -0800

    basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
 from api.v1.auth.auth import Auth  # Moved import here
 from api.v1.views import app_views
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,7 +15,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 auth_type = getenv("AUTH_TYPE")
-if auth_type == 'auth':
+if auth_type == 'basic_auth':  # Use BasicAuth if AUTH_TYPE is basic_auth
+    auth = BasicAuth()
+else:
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for basic authentication.
+"""
+from .auth import Auth
+
+class BasicAuth(Auth):
+    """Class for managing Basic authentication"""
+    pass
