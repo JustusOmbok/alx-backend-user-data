4353a729af6006e6aad400faaebd4c22265c5992
JustusOmbok <ombokjustus@gmail.com>
2024-02-15 00:11:53 -0800

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module  for creating session authentication.
+"""
+from .auth import Auth
+class SessionAuth(Auth):
+    """Create a class SessionAuth that inherits from Auth.
+    """
+    pass
