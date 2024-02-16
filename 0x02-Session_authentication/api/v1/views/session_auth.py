fe9b9243b52db3f0b69bb2bf5f05ce25307a1d68
JustusOmbok <ombokjustus@gmail.com>
2024-02-15 22:38:18 -0800

    session cookies

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -31,3 +31,11 @@ def session_login():
     response.set_cookie(os.getenv("SESSION_NAME"), session_id)
 
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def session_logout():
+    """Deletes session id."""
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
