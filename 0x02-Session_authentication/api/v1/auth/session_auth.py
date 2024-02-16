6d51d63cee2c904b6e573fd9509464473f630459
JustusOmbok <ombokjustus@gmail.com>
2024-02-15 21:00:35 -0800

    session cookies

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,8 @@
 """
 from .auth import Auth
 import uuid
+import os
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -25,3 +27,20 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def session_cookie(self, request=None):
+        """Function that returns session cookies."""
+        if request is None:
+            return None
+        return request.cookies.get(os.getenv("SESSION_NAME"))
+
+    def current_user(self, request=None):
+        """Returns current user session id."""
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        if session_id:
+            user_id = self.user_id_for_session_id(session_id)
+            if user_id:
+                return User.get(user_id)
+        return None
