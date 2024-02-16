14505a16a5d1acc7d4395ab4878dd6eb9d3b341c
JustusOmbok <ombokjustus@gmail.com>
2024-02-15 22:56:49 -0800

    session id deleted

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,7 +4,6 @@
 from flask import jsonify, request
 from api.v1.views import app_views
 from models.user import User
-from api.v1.app import auth
 import os
 
 
@@ -25,6 +24,7 @@ def session_login():
 
     if not user[0].is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
 
     session_id = auth.create_session(user[0].id)
     response = jsonify(user[0].to_json())
@@ -40,6 +40,7 @@ def session_login():
         )
 def session_logout():
     """Deletes session id."""
+    from api.v1.app import auth
     if not auth.destroy_session(request):
         abort(404)
 
