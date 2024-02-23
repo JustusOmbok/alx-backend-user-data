9aae9d5f6d1a0fd97bcdea90f48d36f325aa0c1c
JustusOmbok <ombokjustus@gmail.com>
2024-02-23 05:07:33 -0800

    logout mechanism

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for flask app.
 """
-from flask import Flask, jsonify, request, make_response, abort
+from flask import Flask, jsonify, request, make_response, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -39,6 +39,18 @@ def login():
         return response, 200
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    session_id = request.cookies.get("session_id")
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/", code=302)
+    else:
+        return "Forbidden", 403
 
 
 if __name__ == "__main__":
