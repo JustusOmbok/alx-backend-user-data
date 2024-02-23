62dd13a88a66f5b821e0e328063d3e5f12367b7e
JustusOmbok <ombokjustus@gmail.com>
2024-02-23 00:14:27 -0800

    post user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """Module for flask app.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/")
 def welcome():
     """Function that returns a json."""
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
