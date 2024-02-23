798d7e53f98442e92cdfa9c6929bbf3238e9c8a1
JustusOmbok <ombokjustus@gmail.com>
2024-02-23 04:09:26 -0800

    login mechanism

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,3 +61,14 @@ class Auth:
         except NoResultFound:
             pass
         return None
+
+    def get_user_from_session_id(self, session_id: str):
+        """Get user from session ID"""
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
